13f3c8670265d733a57e305c7d1a62709ec6ab5e
Divyanshu Prince <divyanshuprince764@gmail.com>
2024-10-04 12:24:43 +0530

    update

--- Numpy/1.py
+++ Numpy/1.py
@@ -1,7 +1,7 @@
 import numpy as np
 # Numpy - Numeric Python
 l = [1,2,3,4]
-print(l)
+# print(l)
 
 arr1 = np.array([1,2,3,4,5,6,7,8,9,10])
 # print(arr1)
@@ -28,7 +28,18 @@ arr4 = np.full((3, 5), 5, dtype=int)
 
 
 arr5 = np.array(l)
-print(arr5)
-l2 = [1, "I", "LOVE", "INDIA", 1947, True]
+# print(arr5)
+l2 = ["I", "LOVE", "INDIA", 1947, True]
 arr5 = np.array(l2)
-print(arr5)
+# print(arr5)
+
+
+# print(arr1[2:6])
+# print(arr1[-5:8])
+# print(arr1[5:-8])
+
+
+# arr6 = np.array([[10,11,12,13,14,15,16], [0,1,2,3,4,5,6,7,8,9]], dtype=object)
+arr6 = np.array([[10,11,12,13,14,15,16], [0,1,2,3,4,5,6]])
+print(arr6[1,5])  # print(arr6[1][5])
+print(arr6[0:1, 2:5])
