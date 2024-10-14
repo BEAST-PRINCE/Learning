9ad3574f6169f0468d23145e9d498b6cf51638bb
Divyanshu Prince <divyanshuprince764@gmail.com>
2024-10-14 20:27:00 +0530

    Array slices

--- Golang/2.go
+++ Golang/2.go
@@ -1,14 +1,12 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
 	// Arrays
 
-	// var arr [3]string
-	// fmt.Println(arr)
+	var arr [3]string
+	fmt.Println(arr)
 
 	arr2 := []string{"a", "b", "c", "d", "e"}
 	fmt.Println(len(arr2), arr2)
@@ -16,4 +14,16 @@ func main() {
 	for i := 0; i < len(arr2); i++ {
 		fmt.Println(arr2[i])
 	}
+
+	var s []string = arr2[1:4]
+	fmt.Println(len(s), s)
+	fmt.Println(s[:cap(s)])
+
+	var arr3 []int = []int{1, 2, 3, 4}
+	arr3 = append(arr3, 5)
+	fmt.Println(len(arr3), arr3)
+
+	arr4 := make([]int, 8) // Make created a Slice
+	fmt.Printf("%T\n", arr4)
+	fmt.Println(len(arr4), arr4)
 }
